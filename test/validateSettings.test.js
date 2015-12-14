0becf05c30c361b763debe482c553c56871b4f9c
Alex Fomin <ShurickFomin@gmail.com>
2015-12-14 20:14:53 +0200

    Add travis integration

--- test/validateSettings.test.js
+++ test/validateSettings.test.js
@@ -1,47 +1,48 @@
 var expect = require("chai").expect;
 
 var validateSettings = require("../src/validateSettings").validateSettings;
-
-describe('Validate Settings', function () {
-    it('should validate simple types', ()=> {
-        var settings = {
-            string: 'some string',
-            list: [1]
-        };
-        expect(
-            validateSettings({
-                'string': {
-                    type: 'string'
-                },
-                'list': {
-                    type: 'list'
-                }
+describe('tool-bootstrap', ()=> {
+    describe('Validate Settings', function () {
+        it('should validate simple types', ()=> {
+            var settings = {
+                string: 'some string',
+                list: [1]
+            };
+            expect(
+                validateSettings({
+                    'string': {
+                        type: 'string'
+                    },
+                    'list': {
+                        type: 'list'
+                    }
+                }, settings).next().value)
+                .to.be.eql({accountConfig: settings});
+        });
+        it('should validate urls', ()=> {
+            var settings = {
+                url: 'http://url'
+            };
+            expect(
+                validateSettings({
+                    'url': {
+                        type: 'uri'
+                    }
+                }, settings).next().value)
+                .to.be.eql({accountConfig: settings});
+        });
+        it('should fail on invalid urls', ()=> {
+            var settings = {
+                url: 'fail'
+            };
+            expect(validateSettings({
+                url: {type: 'uri'}
             }, settings).next().value)
-            .to.be.eql({accountConfig: settings});
-    });
-    it('should validate urls', ()=> {
-        var settings = {
-            url: 'http://url'
-        };
-        expect(
-            validateSettings({
-                'url': {
-                    type: 'uri'
-                }
-            }, settings).next().value)
-            .to.be.eql({accountConfig: settings});
-    });
-    it('should fail on invalid urls', ()=> {
-        var settings = {
-            url: 'fail'
-        };
-        expect(validateSettings({
-            url: {type: 'uri'}
-        }, settings).next().value)
-            .to.be.eql({
-            "error": [
-                "'url' has invalid type, should be uri"
-            ]
+                .to.be.eql({
+                "error": [
+                    "'url' has invalid type, should be uri"
+                ]
+            })
         })
     })
 });
